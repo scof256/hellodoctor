a1fce01a9d2ac998d916a6e587f73fe716b4ea2d
scof256 <alscof@gmail.com>
2026-01-19 03:01:24 +0300

    Fix Vercel build: simplify next.config and remove custom webpack config

--- next.config.js
+++ next.config.js
@@ -20,43 +20,6 @@ const nextConfig = {
   
   // Optimize production builds
   productionBrowserSourceMaps: false,
-  
-  // Configure webpack for optimal code splitting
-  webpack: (config, { isServer }) => {
-    // Optimize chunk splitting for better caching
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          ...config.optimization.splitChunks,
-          cacheGroups: {
-            ...config.optimization.splitChunks?.cacheGroups,
-            // Separate vendor chunks for better caching
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all',
-              priority: 10,
-            },
-            // Separate heavy libraries into their own chunks
-            markdown: {
-              test: /[\\/]node_modules[\\/](react-markdown|remark|unified|micromark)[\\/]/,
-              name: 'markdown',
-              chunks: 'all',
-              priority: 20,
-            },
-            qrcode: {
-              test: /[\\/]node_modules[\\/]qrcode[\\/]/,
-              name: 'qrcode',
-              chunks: 'all',
-              priority: 20,
-            },
-          },
-        },
-      };
-    }
-    return config;
-  },
 };
 
 export default withBundleAnalyzer(nextConfig);
